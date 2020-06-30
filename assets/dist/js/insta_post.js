74874b76400bea17e7840607ba20193f2b33c660
Kekoa Wong <kekoawong@MacBook-Pro.hitronhub.home>
2020-06-30 16:16:34 -0700

    Connected Instagram feed to site, need to refresh access token

---
+++ assets/dist/js/insta_post.js
@@ -0,0 +1,11 @@
+var feed = new Instafeed({
+    accessToken: "IGQVJYSTBZAUVk3MzRUM3VnVGswa2FkS2dnTi1DWDBVRGI1MzlPeTRva2hPd3FoUTByd285d2dzbGsyRTFad1VmMGRBZAm9rbWpEVnNpeTNtQUNvVmIwdzZAvWmQ2TE02dkUtZAjVZAbDhR",
+    transform: function(item) {
+      var d = new Date(item.timestamp);
+      item.date = [d.getDate(), d.getMonth(), d.getYear()].join('/');
+      return item;
+    },
+    template: `<a title="Visit Post" class="insta-links" href="{{link}}"><img title="Visit Post" src="{{image}}"/><div class="insta-caption"><p><span class="insta-profile">{{model.username}}</span>{{model.caption}}</p></div></a>`
+  });
+  
+feed.run();
