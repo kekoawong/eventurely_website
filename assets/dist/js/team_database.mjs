a7906bf2b4512e21489a82d8a89a329fbf7680da
Kekoa Wong <kekoawong@macbook-pro.dhcp.nd.edu>
2020-08-08 14:56:04 -0400

    Added privacy policy

---
+++ assets/dist/js/team_database.mjs
@@ -0,0 +1,20 @@
+import Member from './Member_class.js'
+
+export const team = {
+
+    "Advisors": [
+        new Member("Kendyl Pettit", "Alumni Advisor", "Computer Science, ND Class of 2020", "Kendyl Pettit is highly motivated entrepreneur focused on designing unique solutions to the world’s most challenging problems by creating opportunities for every person on the planet to achieve more. When she is not working as a program manager for Microsoft, she is advising the Eventurely team or riding her bike around the Pacific Northwest.", "assets/images/headshots/kendyl_petit.jpeg", "https://www.linkedin.com/in/kendyl-pettit-b4004b13b/")
+    ],
+    "Managment": [
+
+    ],
+    "Business": [
+
+    ],
+    "Design": [
+
+    ],
+    "Engineering": [
+
+    ]
+};
